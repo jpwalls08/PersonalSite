879d7bdb1dc9f3c3dcb468857f88640d3991185f
jpwalls08 <jpwalls08@outlook.com>
2022-10-04 15:21:45 -0500

    Add project files.

---
+++ PersonalSite2/PersonalSite2/Controllers/StronglyTypedDataController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using PersonalSite2.Models;
+
+
+namespace PersonalSite2.Controllers
+{
+    public class StronglyTypedDataController : Controller
+    {
+        //Add a field for the Configuration settings in appsettings.json
+        private readonly IConfiguration _config;
+
+        //Add a constructor for our controller that accepts the config info as a parameter
+        public StronglyTypedDataController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
---
+++ PersonalSite2/PersonalSite2/Models/ContactViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PersonalSite2.Models
+{
+    public class ContactViewModel
+    {
+        [Required(ErrorMessage = "*Name is required")] //Makes the field required
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "*Email is required")]
+        [DataType(DataType.EmailAddress)] //Certain formatiing is expected (@ symbol, .com, etc.)
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "*Subject is required")]
+        public string Subject { get; set; }
+
+        [Required(ErrorMessage = "*Message is required")]
+        [DataType(DataType.MultilineText)] //Makes the textbox for this field bigger
+        public string Message { get; set; }
+    }
+}
