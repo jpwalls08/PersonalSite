fa2651995689b33e03df585b7454c15c8f7cdee5
jpwalls08 <jpwalls08@outlook.com>
2022-11-08 08:49:00 -0600

    Edit to CSS and working on functional Contact View and ViewModel

--- PersonalSite2/PersonalSite2/Models/ContactViewModel.cs
+++ PersonalSite2/PersonalSite2/Models/ContactViewModel.cs
@@ -1,14 +1,17 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
 
 namespace PersonalSite2.Models
 {
+    [Keyless]
     public class ContactViewModel
     {
         [Required(ErrorMessage = "*Name is required")] //Makes the field required
         public string Name { get; set; }
 
-        [Required(ErrorMessage = "*Email is required")]
-        [DataType(DataType.EmailAddress)] //Certain formatiing is expected (@ symbol, .com, etc.)
+        [Required(ErrorMessage = "*")]
+        //[DataType(DataType.EmailAddress)] //Certain formatiing is expected (@ symbol, .com, etc.)
+        [EmailAddress(ErrorMessage = "*Must be a valid Email")]
         public string Email { get; set; }
 
         [Required(ErrorMessage = "*Subject is required")]
